460a249d6951400839b55377203dc5ff1c80552a
kingsam91 <lambetgm@gmail.com>
2020-07-20 23:04:09 -0400

    setup first unittest

---
+++ source_test.py
@@ -0,0 +1,10 @@
+from source import Source # Importing the contact class
+
+class TestSource(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the source class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
